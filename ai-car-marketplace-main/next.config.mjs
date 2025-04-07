c25e93b6b0fa774531b89ba294d8757bfc3a7ff8
shubhamRDJ <149148848+shubhamRDJ@users.noreply.github.com>
2025-04-07 11:08:20 +0530

    first commit

---
+++ ai-car-marketplace-main/next.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qhkbxuxmjeyygaolbxrm.supabase.co",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://Vechiql.created.app;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
